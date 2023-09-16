e42484af2590497d471942aeb9eeca416b1d2561
KadUsh2001 <joaohfpmelo@gmail.com>
2023-09-16 11:41:06 +0200

    added first radio, testing

--- premier_league.py
+++ premier_league.py
@@ -1,5 +1,14 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import os
 
-st.title('Premier league teams')
+path = os.getcwd() + "/" + "premier_league.csv"
+
+st.title('Premier league team results')
+
+data = pd.read_csv(path)
+
+def type_of_analysis():
+    type_of_an = st.radio("What type of analysis would you like?", ("Season only", "Team only", "Season and team"))
+    return type_of_an
